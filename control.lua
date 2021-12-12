7b86adbc2cfb2a0d83d25ea49d146e0c8f6f972d
Aaron Veden <veden@vedbyte.com>
2021-12-11 19:11:41 -0800

    optimized evolution calculation

--- control.lua
+++ control.lua
@@ -45,8 +45,8 @@ local function onModSettingsChange(event)
     world.getFlowQuery = {
         name = "",
         input = false,
-        -- precision_index = defines.flow_precision_index.one_second,
-        precision_index = defines.flow_precision_index.one_minute,
+        precision_index = defines.flow_precision_index.five_seconds,
+        -- precision_index = defines.flow_precision_index.one_minute,
         count = true
     }
 
@@ -54,88 +54,130 @@ local function onModSettingsChange(event)
 end
 
 local function onConfigChanged()
-    if not world.version or world.version < 2 then
+    if not world.version or world.version < 3 then
+        world.version = 3
 
         onModSettingsChange()
 
-        for _,p in ipairs(game.connected_players) do
-            p.print("Rampant Evolution - Version 1.0.2")
-        end
-        world.version = 2
+        world.kills = {}
+        world.pollution = {}
+        game.forces.enemy.evolution_factor = 0
+
+        game.print("Rampant Evolution - Version 1.1.0")
     end
 end
 
-local function onTick()
+local function onStatsGrabPollution()
     local enemyForce = game.forces.enemy
     local evo = enemyForce.evolution_factor
-    local query = world.getFlowQuery
-
-    -- print(evo)
+    local negativeEvo = evo
+    local positiveEvo = evo
 
     local pollutionStats = game.pollution_statistics
     local counts = pollutionStats.output_counts
 
-    local pollutionFn = pollutionStats.get_flow_count
-    for name in pairs(counts) do
-        query.name = name
-        if (name == "tile-proxy") and (world.evolutionPerTileAbsorbed < 0) then
-            evo = evo + ((1 - evo) * world.evolutionPerTileAbsorbed * pollutionFn(query))
-        elseif (name == "tree-proxy") and (world.evolutionPerTreeAbsorbed < 0) then
-            evo = evo + ((1 - evo) * world.evolutionPerTreeAbsorbed * pollutionFn(query))
-        elseif (name == "dying-tree-proxy") and (world.evolutionPerTreeDied < 0) then
-            evo = evo + ((1 - evo) * world.evolutionPerTreeDied * pollutionFn(query))
-        elseif (world.evolutionPerSpawnerAbsorbed < 0) and isValidSpawnerConsumer(name) then
-            evo = evo + ((1 - evo) * world.evolutionPerSpawnerAbsorbed * pollutionFn(query))
+    for name,count in pairs(counts) do
+        local previousCount = world.pollution[name]
+        local delta
+        if not previousCount then
+            world.pollution[name] = count
+            delta = count
+        else
+            delta = count - previousCount
         end
-        if (evo < 0) then
-            evo = 0
-        end
-    end
 
-    for name in pairs(counts) do
-        query.name = name
-        if (name == "tile-proxy") and (world.evolutionPerTileAbsorbed > 0) then
-            evo = evo + ((1 - evo) * world.evolutionPerTileAbsorbed * pollutionFn(query))
-        elseif (name == "tree-proxy") and (world.evolutionPerTreeAbsorbed > 0) then
-            evo = evo + ((1 - evo) * world.evolutionPerTreeAbsorbed * pollutionFn(query))
-        elseif (name == "dying-tree-proxy") and (world.evolutionPerTreeDied > 0) then
-            evo = evo + ((1 - evo) * world.evolutionPerTreeDied * pollutionFn(query))
-        elseif (world.evolutionPerSpawnerAbsorbed > 0) and isValidSpawnerConsumer(name) then
-            evo = evo + ((1 - evo) * world.evolutionPerSpawnerAbsorbed * pollutionFn(query))
-        end
-    end
+        if delta ~= 0 then
+            if (name == "tile-proxy") then
+                if (world.evolutionPerTileAbsorbed < 0) then
+                    negativeEvo = negativeEvo + ((1 - negativeEvo) * world.evolutionPerTileAbsorbed * delta)
+                else
+                    positiveEvo = positiveEvo + ((1 - positiveEvo) * world.evolutionPerTileAbsorbed * delta)
+                end
+            elseif (name == "tree-proxy") then
+                if (world.evolutionPerTreeAbsorbed < 0) then
+                    negativeEvo = negativeEvo + ((1 - negativeEvo) * world.evolutionPerTreeAbsorbed * delta)
+                else
+                    positiveEvo = positiveEvo + ((1 - positiveEvo) * world.evolutionPerTreeAbsorbed * delta)
+                end
+            elseif (name == "dying-tree-proxy") then
+                if (world.evolutionPerTreeDied < 0) then
+                    negativeEvo = negativeEvo + ((1 - negativeEvo) * world.evolutionPerTreeDied * delta)
+                else
+                    positiveEvo = positiveEvo + ((1 - positiveEvo) * world.evolutionPerTreeDied * delta)
+                end
 
-    local killStats = enemyForce.kill_count_statistics
-
-    counts = killStats.output_counts
-    local killFn = killStats.get_flow_count
-
-    for name in pairs(counts) do
-        query.name = name
-        if (world.evolutionPerSpawnerKilled < 0) and isValidSpawnerConsumer(name) then
-            evo = evo + ((1 - evo) * world.evolutionPerSpawnerKilled * killFn(query))
-        elseif (world.evolutionPerHiveKilled < 0) and isValidHiveConsumer(name) then
-            evo = evo + ((1 - evo) * world.evolutionPerHiveKilled * killFn(query))
-        elseif (world.evolutionPerUnitKilled < 0) and isValidUnit(name) then
-            evo = evo + ((1 - evo) * world.evolutionPerUnitKilled * killFn(query))
-        end
-        if (evo < 0) then
-            evo = 0
+            elseif isValidSpawnerConsumer(name) then
+                if (world.evolutionPerSpawnerAbsorbed < 0) then
+                    negativeEvo = negativeEvo + ((1 - negativeEvo) * world.evolutionPerSpawnerAbsorbed * delta)
+                else
+                    positiveEvo = positiveEvo + ((1 - positiveEvo) * world.evolutionPerSpawnerAbsorbed * delta)
+                end
+            end
         end
     end
 
-    for name in pairs(counts) do
-        query.name = name
-        if (world.evolutionPerSpawnerKilled > 0) and isValidSpawnerConsumer(name) then
-            evo = evo + ((1 - evo) * world.evolutionPerSpawnerKilled * killFn(query))
-        elseif (world.evolutionPerHiveKilled > 0) and isValidHiveConsumer(name) then
-            evo = evo + ((1 - evo) * world.evolutionPerHiveKilled * killFn(query))
-        elseif (world.evolutionPerUnitKilled > 0) and isValidUnit(name) then
-            evo = evo + ((1 - evo) * world.evolutionPerUnitKilled * killFn(query))
+    local negativeEvoDelta = evo - negativeEvo
+    if negativeEvoDelta < 0 then
+        negativeEvoDelta = 0
+        evo = 0
+    end
+
+    local positiveEvoDelta = positiveEvo - evo
+
+    enemyForce.evolution_factor = (evo - negativeEvoDelta) + positiveEvoDelta
+end
+
+local function onStatsGrabKill()
+    local enemyForce = game.forces.enemy
+    local killStats = game.forces.enemy.kill_count_statistics
+
+    local evo = enemyForce.evolution_factor
+    local negativeEvo = evo
+    local positiveEvo = evo
+    local counts = killStats.output_counts
+
+    for name,count in pairs(counts) do
+        local previousCount = world.kills[name]
+        local delta
+        if not previousCount then
+            world.kills[name] = count
+            delta = count
+        else
+            delta = count - previousCount
+        end
+
+        if delta ~= 0 then
+            if isValidSpawnerConsumer(name) then
+                if (world.evolutionPerSpawnerKilled < 0) then
+                    negativeEvo = negativeEvo + ((1 - negativeEvo) * world.evolutionPerSpawnerKilled * delta)
+                else
+                    positiveEvo = positiveEvo + ((1 - positiveEvo) * world.evolutionPerSpawnerKilled * delta)
+                end
+            elseif isValidHiveConsumer(name) then
+                if (world.evolutionPerHiveKilled < 0) then
+                    negativeEvo = negativeEvo + ((1 - negativeEvo) * world.evolutionPerHiveKilled * delta)
+                else
+                    positiveEvo = positiveEvo + ((1 - positiveEvo) * world.evolutionPerHiveKilled * delta)
+                end
+            elseif isValidUnit(name) then
+                if (world.evolutionPerUnitKilled < 0) then
+                    negativeEvo = negativeEvo + ((1 - negativeEvo) * world.evolutionPerUnitKilled * delta)
+                else
+                    positiveEvo = positiveEvo + ((1 - positiveEvo) * world.evolutionPerUnitKilled * delta)
+                end
+            end
         end
     end
 
-    enemyForce.evolution_factor = evo
+    local negativeEvoDelta = evo - negativeEvo
+    if (negativeEvoDelta < 0) then
+        negativeEvoDelta = 0
+        evo = 0
+    end
+
+    local positiveEvoDelta = positiveEvo - evo
+
+    enemyForce.evolution_factor = (evo - negativeEvoDelta) + positiveEvoDelta
 end
 
 local function onInit()
@@ -152,8 +194,9 @@ end
 
 -- hooks
 
-script.on_nth_tick(60*60, onTick)
--- script.on_nth_tick(60, onTick)
+script.on_nth_tick((5*60*60)+0, onStatsGrabPollution)
+script.on_nth_tick((5*60*60)+1, onStatsGrabKill)
+
 script.on_init(onInit)
 script.on_load(onLoad)
 script.on_event(defines.events.on_runtime_mod_setting_changed, onModSettingsChange)
